7e75e2698305a9dc105ab7029572eec0802b4fae
Daniel García <nelo686@gmail.com>
2024-09-25 20:17:10 +0200

    MovieDetailViewModel test has been added

---
+++ app/src/test/java/es/mrmoustard/tmdbco/ui/screen/detail/MovieDetailViewModelTest.kt
@@ -0,0 +1,49 @@
+package es.mrmoustard.tmdbco.ui.screen.detail
+
+import androidx.lifecycle.SavedStateHandle
+import arrow.core.right
+import es.mrmoustard.data.repository.MoviesRepository
+import es.mrmoustard.domain.model.MovieDetail
+import es.mrmoustard.domain.model.TopRatedWrapper
+import es.mrmoustard.tmdbco.ui.MainDispatcherRule
+import es.mrmoustard.tmdbco.ui.screen.toprated.TopRatedViewModel
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+class MovieDetailViewModelTest {
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private lateinit var viewModel: MovieDetailViewModel
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+    }
+
+    @Test
+    fun `GIVEN a movie id, WHEN call getMovieDetails(), THEN movie details are retrieved`() =
+        runBlocking {
+            // Given
+            val savedStateHandle = mock<SavedStateHandle>()
+            val repository = mock<MoviesRepository>()
+            whenever(repository.getMovieDetails(any())).thenReturn(MovieDetail().right())
+
+            // When
+            viewModel = MovieDetailViewModel(savedStateHandle, repository)
+
+            // Then
+            assertTrue(viewModel.state.movie.isRight())
+            assertThat(viewModel.state.movie.getOrNull(), instanceOf(MovieDetail::class.java))
+        }
+}
