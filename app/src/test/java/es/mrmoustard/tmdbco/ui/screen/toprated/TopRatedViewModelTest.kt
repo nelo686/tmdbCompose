ee8056a786f9d4f319f57c5c02bde872878dd6f9
Daniel García <nelo686@gmail.com>
2024-10-03 00:50:24 +0200

    refactor

--- app/src/test/java/es/mrmoustard/tmdbco/ui/screen/toprated/TopRatedViewModelTest.kt
+++ app/src/test/java/es/mrmoustard/tmdbco/ui/screen/toprated/TopRatedViewModelTest.kt
@@ -1,23 +1,24 @@
 package es.mrmoustard.tmdbco.ui.screen.toprated
 
-import arrow.core.left
 import arrow.core.right
 import es.mrmoustard.data.repository.MoviesRepository
-import es.mrmoustard.data.source.remote.dto.Error
-import es.mrmoustard.domain.model.TopRatedWrapper
+import es.mrmoustard.domain.model.Movie
+import es.mrmoustard.tmdbco.mock.movieMock
+import es.mrmoustard.tmdbco.mock.topRatedWrapperMock
 import es.mrmoustard.tmdbco.ui.MainDispatcherRule
 import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.hasItems
 import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.any
-import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 
 @RunWith(MockitoJUnitRunner::class)
@@ -25,6 +26,8 @@ class TopRatedViewModelTest {
 
     @get:Rule
     val mainDispatcherRule = MainDispatcherRule()
+
+    @Mock lateinit var repository: MoviesRepository
 
     private lateinit var viewModel: TopRatedViewModel
 
@@ -37,29 +40,14 @@ class TopRatedViewModelTest {
     fun `GIVEN a page number, WHEN call getTopRated(), THEN a movie list is retrieved`() =
         runBlocking {
             // Given
-            val repository = mock<MoviesRepository>()
-            whenever(repository.getTopRated(any(), any())).thenReturn(TopRatedWrapper().right())
+            whenever(repository.getTopRated(any(), any())).thenReturn(topRatedWrapperMock.right())
 
             // When
             viewModel = TopRatedViewModel(repository)
 
             // Then
             assertTrue(viewModel.state.movies.isRight())
-            assertThat(viewModel.state.movies.getOrNull(), instanceOf(TopRatedWrapper::class.java))
-        }
-
-    @Test
-    fun `GIVEN a page number, WHEN call getTopRated(), but THEN a unexpected error is retrieved`() =
-        runBlocking {
-            // Given
-            val repository = mock<MoviesRepository>()
-            whenever(repository.getTopRated(any(), any())).thenReturn(Error.Unknown("unknown").left())
-
-            // When
-            viewModel = TopRatedViewModel(repository)
-
-            // Then
-            assertTrue(viewModel.state.movies.isLeft())
-            assertThat(viewModel.state.movies.leftOrNull(), instanceOf(Error::class.java))
+            assertThat(viewModel.state.movies.getOrNull(), hasItems(movieMock))
+            assertThat(viewModel.state.movies.getOrNull()?.first(), instanceOf(Movie::class.java))
         }
 }
